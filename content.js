102be413a88c7cd8a4c36dded7fb9db0f99e5db0
Yehia Ali <42983220+yehia1ali@users.noreply.github.com>
2021-11-22 19:27:49 +0200

    Update content.js

--- content.js
+++ content.js
@@ -1,42 +1,40 @@
 
-
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
 
-if (request.message === 'TabUpdated' ) {
-
-document.querySelector('[aria-label*="comment"],[aria-label*="تعليق"]').addEventListener("click", function (evt) {
-
+  if (request.message === 'TabUpdated' ) {
   
+  document.querySelector('[aria-label*="comment"],[aria-label*="تعليق"]').addEventListener("click", function (evt) {
+
+
+
+setTimeout(function(){
+
+
+  document.querySelectorAll('[dir="auto"],[dir="ltr"]').forEach((Element) => {
+  // https://stackoverflow.com/questions/4446244/how-to-check-if-any-arabic-character-exists-in-the-string-javascript/4446309
   
- setTimeout(function(){
-
-
-    
-document.querySelectorAll('[dir="auto"],[dir="ltr"]').forEach((Element) => {
-// https://stackoverflow.com/questions/4446244/how-to-check-if-any-arabic-character-exists-in-the-string-javascript/4446309
-
-let arabic = /[\u0600-\u06FF]/,
-string = Element.textContent;
-
-if(arabic.test(string)){
-Element.setAttribute("dir", "rtl");
-}
-
-});
-
-return false;
-
-});
+  let arabic = /[\u0600-\u06FF]/,
+  string = Element.textContent;
   
-  
-  
-}, 500);//wait 0.5 seconds
-  
-
-
-
-
-
-
+  if(arabic.test(string)){
+  Element.setAttribute("dir", "rtl");
   }
-})
+  
+  });
+  
+  evt.preventDefault();
+  
+  });
+    
+    
+    
+  }, 500);//wait 0.5 seconds
+    
+  
+  
+  
+  
+  
+  
+    }
+  })
