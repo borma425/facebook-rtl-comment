ecb07727ad51b3efd5102099e52170221072dbcf
Yehia Ali <42983220+yehia1ali@users.noreply.github.com>
2021-11-22 19:10:42 +0200

    Update content.js

--- content.js
+++ content.js
@@ -1,26 +1,40 @@
 
+
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
 
 if (request.message === 'TabUpdated' ) {
 
+document.querySelector('[aria-label*="comment"],[aria-label*="تعليق"]').addEventListener("click", function (evt) {
+
+  
+  
+ setTimeout(function(){
+
+
+    
 document.querySelectorAll('[dir="auto"],[dir="ltr"]').forEach((Element) => {
 // https://stackoverflow.com/questions/4446244/how-to-check-if-any-arabic-character-exists-in-the-string-javascript/4446309
 
 let arabic = /[\u0600-\u06FF]/,
 string = Element.textContent;
 
-
-setTimeout(function(){
-
 if(arabic.test(string)){
 Element.setAttribute("dir", "rtl");
 }
-  
-}, 500);//wait 0.5 seconds
-
-
 
 });
+
+return false;
+
+});
+  
+  
+  
+}, 500);//wait 0.5 seconds
+  
+
+
+
 
 
 
