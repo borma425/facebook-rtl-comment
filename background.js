8b9dc8bd27cf8f439c010228ca343d2ccd1d8eab
marwanzayed-coder <marwanzayedshaban@gmail.com>
2022-10-20 21:02:47 +0200

    Converting from manifest 2 to manifest 3 and solving problems and formatting code

--- background.js
+++ background.js
@@ -1,15 +1,9 @@
-chrome.runtime.onInstalled.addListener(function() {
-
-  chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-
-    if (changeInfo.status === 'complete') {
-
-
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+    if (changeInfo.status === "complete") {
       chrome.tabs.sendMessage(tabId, {
-        message: 'TabUpdated'
+        message: "TabUpdated",
       });
-
-
     }
-  })
+  });
 });
